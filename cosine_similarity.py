d77704ab184a987caaae9dbc6abb6cfa3696d56c
CUKykkim <ykkim77@cuk.edu>
2023-05-23 11:59:29 +0900

    s

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -5,12 +5,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 vectorizer = TfidfVectorizer()
 
 # 한국어 문장들
-sentence1 = "저는 오늘 밥을 먹었습니다."
-sentence2 = "저는 어제 밥을 먹었습니다."
+sentence1 = "나는 학교에 갔다"
+sentence2 = "나는 영화관에 갔다"
 # sentence2 = "너는 그제 밥을 지었습니다."
 
 # 문장들을 벡터화
 tfidf_matrix = vectorizer.fit_transform([sentence1, sentence2])
+print(tfidf_matrix)
 
 # 문장1과 문장2의 코사인 유사도 계산
 cosine_sim = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
